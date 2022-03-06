5e9021e7f7ae829c4dd5a457bebc1556489d54d2
rokuosan <85651386+rokuosan@users.noreply.github.com>
2022-03-07 00:57:12 +0900

    Memoryエンティティを作成

---
+++ src/main/kotlin/com/deviseworks/mccBackend/domain/Memory.kt
@@ -0,0 +1,17 @@
+package com.deviseworks.mccBackend.domain
+
+import kotlinx.serialization.SerialName
+
+@kotlinx.serialization.Serializable
+data class Memory(
+    @SerialName("total_memory")
+    val totalMemory: Long,
+    @SerialName("max_memory")
+    val maxMemory: Long,
+    @SerialName("free_memory")
+    val freeMemory: Long,
+    @SerialName("used_memory")
+    val usedMemory: Long,
+    @SerialName("ratio")
+    val ratio: Double
+)
