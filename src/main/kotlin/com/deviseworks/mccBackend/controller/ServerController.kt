f65c59a86bc85cf854252c8ae50f2eec0eccb876
rokuosan <85651386+rokuosan@users.noreply.github.com>
2022-03-07 01:14:46 +0900

    /api/server/memoryのPOSTに対応

--- src/main/kotlin/com/deviseworks/mccBackend/controller/ServerController.kt
+++ src/main/kotlin/com/deviseworks/mccBackend/controller/ServerController.kt
@@ -1,9 +1,11 @@
 package com.deviseworks.mccBackend.controller
 
 import com.deviseworks.mccBackend.domain.Memory
+import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.RestController
 
@@ -13,25 +15,18 @@ class ServerController {
     // Server VM
     val runtime: Runtime = Runtime.getRuntime()
 
+    // Server Memory
+    var serverMemory = Memory()
+
     @GetMapping("/api/server/memory")
     @ResponseBody
     fun getServerMemory(): String{
-        // Get memory size
-        val totalKB = runtime.totalMemory() / 1024
-        val maxKB = runtime.maxMemory() / 1024
-        val freeKB = runtime.freeMemory() / 1024
-        val usedKB = totalKB - freeKB
-        val ratio: Double = (usedKB / totalKB.toDouble()) * 100
-
-        // Serialize
-        val memory = Memory(totalKB, maxKB, freeKB, usedKB, ratio)
-
-        // Encode
-        return Json.encodeToString(Memory.serializer(), memory)
+        return Json.encodeToString(Memory.serializer(), serverMemory)
     }
 
     @PostMapping("/api/server/memory")
-    fun updateServerMemory(){
-
+    fun updateServerMemory(@RequestBody body: String){
+        val json = Memory().parse(body)?: return
+        serverMemory=json
     }
 }
--- src/main/kotlin/com/deviseworks/mccBackend/domain/Memory.kt
+++ src/main/kotlin/com/deviseworks/mccBackend/domain/Memory.kt
@@ -1,17 +1,27 @@
 package com.deviseworks.mccBackend.domain
 
 import kotlinx.serialization.SerialName
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
 
 @kotlinx.serialization.Serializable
 data class Memory(
     @SerialName("total_memory")
-    val totalMemory: Long,
+    val totalMemory: Long = 0,
+
     @SerialName("max_memory")
-    val maxMemory: Long,
+    val maxMemory: Long = 0,
+
     @SerialName("free_memory")
-    val freeMemory: Long,
+    val freeMemory: Long = 0,
+
     @SerialName("used_memory")
-    val usedMemory: Long,
+    val usedMemory: Long = 0,
+
     @SerialName("ratio")
-    val ratio: Double
-)
+    val ratio: Double = 0.0
+){
+    fun parse(body: String): Memory?{
+        return Json.decodeFromString(body)
+    }
+}
