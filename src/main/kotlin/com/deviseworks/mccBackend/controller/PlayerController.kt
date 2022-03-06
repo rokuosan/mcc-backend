b5c6b79ef46b921271502549d034dd84d501af29
rokuosan <85651386+rokuosan@users.noreply.github.com>
2022-03-07 00:56:56 +0900

    PlayerのEntityとControllerを作成

---
+++ src/main/kotlin/com/deviseworks/mccBackend/controller/PlayerController.kt
@@ -0,0 +1,45 @@
+package com.deviseworks.mccBackend.controller
+
+import com.deviseworks.mccBackend.domain.Player
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class PlayerController {
+
+    val onlineList = mutableListOf<Player>()
+
+
+    @GetMapping("/api/player/list")
+    @ResponseBody
+    fun getPlayerList(): String{
+        // とりあえずonlineList返しとくか
+        return onlineList.toString()
+    }
+
+    @PostMapping("/api/player/list/{status}")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    fun updatePlayerList(
+        @PathVariable status: String,
+        @RequestBody body: String
+    ){
+        println(body)
+        // 受け取ったBodyをJSONにパースして取得したユーザをonlineListに追加する
+        val player = Player().parse(body)?:return
+
+        when(status){
+            "join" ->{
+                onlineList.add(player)
+            }
+            "quit" ->{
+                onlineList.remove(player)
+            }
+        }
+    }
+}
--- src/main/kotlin/com/deviseworks/mccBackend/domain/Player.kt
+++ src/main/kotlin/com/deviseworks/mccBackend/domain/Player.kt
@@ -1,23 +1,50 @@
 package com.deviseworks.mccBackend.domain
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
 import org.jetbrains.annotations.NotNull
 import javax.persistence.Column
 import javax.persistence.Entity
 import javax.persistence.Id
 import javax.persistence.Table
 
+@kotlinx.serialization.Serializable
 @Entity
 @Table(name="players")
 data class Player(
+    // UUID
     @Id
     @NotNull
     @Column(name="uuid")
+    @SerialName("uuid")
     var uuid: String = "",
+
+    // Name
     @NotNull
+    @SerialName("name")
     @Column(name="name")
     var name: String = "",
+
+    @SerialName("display_name")
     @Column(name="display_name")
     var displayName: String? = null,
+
+    @SerialName("admin_flag")
     @Column(name="admin_flag")
     var isAdmin: Boolean = false
-)
+){
+    fun parse(body: String): Player? {
+        return Json.decodeFromString(body)
+    }
+}
+
+/*
+Expected JSON Example:
+{
+    "uuid": "This is UUID v4.",
+    "name": "Foo",
+    "display_name": "Bar",
+    "admin_flag": false
+}
+ */
